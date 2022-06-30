4df44bfd7e7f690ad8d4ef74f123af6cecdee26e
Tomydoggy <108366896+Tomydoggy@users.noreply.github.com>
2022-06-30 12:58:19 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,16 +19,16 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['Your Number']
-global.premium = ['Your Number']
-global.ownernomer = 'Your Number'
-global.ownername = '🐦Star Boy Nexus'
+global.owner = ['+94702410162']
+global.premium = ['+94702410162']
+global.ownernomer = '+94759094181'
+global.ownername = '🐦SHIYAM BOY'
 global.botname = 'Gojo-Satoru'
 global.footer = '©NexusNwInc.'
 global.ig = 'https://github.com/nexusNw'
-global.region = 'India, South India, Kerala'
+global.region = 'SRI LANKA , SINHALA'
 global.sc = 'https://github.com/nexusNw/Gojo-Satoru'
-global.myweb = 'https://youtube.com/channel/UCqoUjPvDdb0kjXNYdvPPpHQ'
+global.myweb = ''
 global.packname = 'Gojo-Satoru'
 global.author = '🐦Star Boy Nexus'
 global.sessionName = 'session'
